1b179241ce3e414b7638d11e7e376c7e85901f5a
achicodes?? <265846161+achicodess@users.noreply.github.com>
2026-05-20 21:48:12 +0530

    Added a menu and information about what each option does after selection.
    Added a menu for player options after validating input and information about what each section is about.

--- basic_code.py
+++ basic_code.py
@@ -1,9 +1,34 @@
 print("Players in this draft: LeBron James, Stephen Curry, Luka Dončić and LaMelo")
-print("Please enter names in lowercase")
-name=input("Enter your favorite NBA player: ").strip() .lower()
-players=["lebron james", "lebron", "stephen curry", "curry", "luka dončić", "luka", "lamelo"] 
-#Only lowercase letters allowed. 
+print("*ATTENTION*: Please enter lowercase letters only!!")
+
+name = input("Enter your favorite NBA player: ").strip().lower()
+
+players = ["lebron james", "lebron", "stephen curry", "curry", "luka dončić", "luka", "lamelo"]
+
 if name in players:
-	print("Player found.")
+    print("Player found.")
+    while True:
+        print("Option 1 - Match scores")
+        print("Option 2 - Statistics")
+        print("Option 3 - Achievements")
+        print("Option 4 - Exit")
+
+        try:
+            opt = int(input("Enter your choice: "))
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+
+        if opt == 1:
+            print("This section gives you the baskets scored by", name, "in recent matches.")
+        elif opt == 2:
+            print("This section gives you an overall summary of how your chosen player has played across their entire career.")
+        elif opt == 3:
+            print("This section tells you about the achievements of", name)
+        elif opt == 4:
+            print("Exiting the application.")
+            break
+        else:
+            print("Invalid input")
 else:
-	print("Please refer the players list above.")
+    print("Player not found. Please refer to the players list above.")
