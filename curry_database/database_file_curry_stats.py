5e4f5ab993738ccd355a23d41a3642de6586350e
achicodes?? <265846161+achicodess@users.noreply.github.com>
2026-05-23 21:51:06 +0530

    Adding the 3rd file that got deleted previously

---
+++ curry_database/database_file_curry_stats.py
@@ -0,0 +1,6 @@
+Stephen Curry averaged 26.6 points, 4.7 assists, and 3.6 rebounds per game while shooting 46.8% from the field and 39.3% from three-point range during the 2025–26 NBA regular season.
+* April 18, 2026 (vs. Suns): 17 PTS, 4 REB, 4 AST, 2 STL (36 MIN)
+* April 15, 2026 (vs. Clippers): 35 PTS, 4 AST, 1 REB, 1 STL (36 MIN)
+* April 13, 2026 (vs. Clippers): 24 PTS, 6 REB, 3 AST, 1 STL (29 MIN)
+* April 11, 2026 (vs. Kings): 11 PTS, 5 AST, 3 REB, 2 STL (27 MIN)
+* April 8, 2026 (vs. Kings): 17 PTS, 5 REB, 2 AST, 1 STL (25 MIN)
